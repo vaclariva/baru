3739e37f1146fe0482bd368fa5c80feb5d444933
vaclariva <meyclariva@gmail.com>
2023-12-19 23:21:39 +0700

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import Landing from './pages/Landing';
+import DetailPage from './pages/DetailPage';
+import Layout from './components/Layout';
+import JobPage from './pages/JobPage';
+import Profile from './pages/Profile';
+import Upload from './pages/Upload';
+import Login from "./pages/Auth/Login/Login";
+import Register from "./pages/Auth/Registrasi/Register";
+import User from "./admin/User";
+import Admin from "./admin/Admin";
+import Daftar from "./admin/Authentifikasi/Daftar/Daftar";
+import Masuk from "./admin/Authentifikasi/Masuk/Masuk";
+import Home from "./admin/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Landing />,
+      },
+      {
+        path: '/landing',
+        element: <LandingPage />,
+      },
+      {
+        path: '/job',
+        element: <JobPage />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
+      {
+        path: '/job/:id',
+        element: <DetailPage />,
+      },
+      {
+        path: '/profile/:id',
+        element: <Profile />,
+      },
+      {
+        path: '/upload',
+        element: <Upload />,
+      },
+      {
+        path: '/user',
+        element: <User />,
+      },
+      {
+        path: '/admin',
+        element: <Admin />,
+      },
+      {
+        path: '/daftar',
+        element: <Daftar />,
+      },
+      {
+        path: '/masuk',
+        element: <Masuk />,
+      },
+      {
+        path: '/home',
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default router;
